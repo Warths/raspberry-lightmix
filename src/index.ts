7473959fcc0850334bf257f949343113c168778b
Warths <pro.warths@gmail.com>
2024-08-06 21:45:48 +0200

    Minimal keep-alive code

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log("Test")
+// app.ts
+function mainLoop() {
+    console.log("The application is running. Timestamp:", new Date().toISOString());
+}
+
+// Set an interval to run the mainLoop function every 1000 milliseconds (1 second)
+setInterval(mainLoop, 1000);
