57e610809e3299268d85febaefcf31b85a15f8ad
Warths <pro.warths@gmail.com>
2024-08-18 16:30:09 +0200

    Tests hardware

--- src/classes/peripherals/RGBWYV.ts
+++ src/classes/peripherals/RGBWYV.ts
@@ -1,16 +1,21 @@
 import { Fixture } from "../lightmix/fixture";
 
+import * as i2c from "i2c-bus";
+
 export class RGBWYV {
+    bus:  i2c.I2CBus
     constructor(
         private i2cBus: any, 
         private address: number, 
         private fixture: Fixture
-    ) {}
+    ) {
+        this.bus = i2c.openSync(1)
+    }
 
     writeState() {
         this.fixture.getState()
         const state = (Math.random() * 255).toFixed(0)
         console.log(state)
-        this.i2cBus.writeByteSync(this.address, 0x00, 0x10)
+        this.i2cBus.writeByteSync(this.address, 0x01, state)
     }
 }
